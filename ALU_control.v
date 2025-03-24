9eb672cb99749d75c4a38724ba1c889679768220
Priyanshu7900 <164048870+Priyanshu7900@users.noreply.github.com>
2025-03-24 07:42:04 +0530

    Create ALU_control.v

---
+++ ALU_control.v
@@ -0,0 +1,22 @@
+//ALU control
+
+
+module ALUcontrol(ALUop,fun7,fun3,Control_out);
+input fun7;
+input[2:0]fun3;
+input[1:0]ALUop;
+output reg[3:0]Control_out;
+
+always@(*)
+begin
+case({ALUop,fun7,fun3})
+6'b000_000:Control_out=4'b0010;
+6'b010_000:Control_out=4'b0110;
+6'b100_000:Control_out=4'b0010;
+6'b101_000:Control_out=4'b0110;
+6'b100_111:Control_out=4'b0000;
+6'b100_110:Control_out=4'b0001;
+default:Control_out=4'b0;
+endcase
+end
+endmodule
