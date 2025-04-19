540d84b78859feee8c40ee397a3744b8049246b1
Priyanshu7900 <164048870+Priyanshu7900@users.noreply.github.com>
2025-04-19 14:27:14 +0530

    Update control_unit.v

--- control_unit.v
+++ control_unit.v
@@ -9,10 +9,11 @@ always@(*)
 begin
 case(Instruction)
 7'b0110011:{ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,ALUOp}=8'b00100010;
-7'b0000011:{ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,ALUOp}=8'b11110000;
-7'b0100011:{ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,ALUOp}=8'b10001000;
+7'b0010011:{ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,ALUOp}=8'b10100000;
+7'b0000011:{ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,ALUOp}=8'b01110000;
+7'b0100011:{ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,ALUOp}=8'b00001000;
 7'b1100011:{ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,ALUOp}=8'b00000101;
-default:{ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,ALUOp}=8'b0;
+default:{ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,ALUOp}=8'b00000000;
 endcase
 end
 endmodule
