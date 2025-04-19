ceac6f1378dd79b6d2c3b2aefad78b2cd223d339
Priyanshu7900 <164048870+Priyanshu7900@users.noreply.github.com>
2025-04-19 14:30:13 +0530

    Update data_memory.v

--- data_memory.v
+++ data_memory.v
@@ -1,20 +1,23 @@
-//Data memory
-
-module Datamemmory(clk,reset,Address,Write_data,Mem_write,Mem_read,MemData_out);
+module Datamemmory(clk,reset,Address,Write_data,Mem_write,Mem_read,MemData_out,dm1,dm2);
 input clk,reset,Mem_read,Mem_write;
 input[31:0]Address,Write_data;
-output [31:0] MemData_out;
+output [31:0] MemData_out,dm1,dm2;
 reg[31:0]D_memory[63:0];
 integer k;
-always@(posedge clk or posedge reset)
-begin
-if(reset)
+initial
 begin
 for(k=0;k<64;k=k+1)
-D_memory[k]<=32'b0;
+D_memory[k]=2*k;
 end
-else if(Mem_write)
-D_memory[Address>>2]<=Write_data;
+always@(posedge clk )
+begin
+ if(Mem_write)
+D_memory[Address]<=Write_data;
 end
 assign MemData_out=(Mem_read)?D_memory[Address]:32'b0;
+
+ assign dm1=D_memory[37];
+  assign dm2=D_memory[58];
+
+
 endmodule
