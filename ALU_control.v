ea61097e0aafeb16fd3310126d8b5826136203a9
Priyanshu7900 <164048870+Priyanshu7900@users.noreply.github.com>
2025-04-19 14:29:24 +0530

    Update ALU_control.v

--- ALU_control.v
+++ ALU_control.v
@@ -1,6 +1,3 @@
-//ALU control
-
-
 module ALUcontrol(ALUop,fun7,fun3,Control_out);
 input fun7;
 input[2:0]fun3;
@@ -10,13 +7,14 @@ output reg[3:0]Control_out;
 always@(*)
 begin
 case({ALUop,fun7,fun3})
-6'b000_000:Control_out=4'b0010;
+6'b000_010:Control_out=4'b0010;
+6'b000_011:Control_out=4'b0011;
 6'b010_000:Control_out=4'b0110;
 6'b100_000:Control_out=4'b0010;
 6'b101_000:Control_out=4'b0110;
 6'b100_111:Control_out=4'b0000;
 6'b100_110:Control_out=4'b0001;
-default:Control_out=4'b0;
+default:Control_out=4'b0010;
 endcase
 end
 endmodule
